c6c4c0a00a972b12fb157e53348928f60a6e8209
drphe <mmail.phe@gmail.com>
2023-10-22 23:45:22 +0700

    ver 1.0

--- background.js
+++ background.js
@@ -1,10 +1,3 @@
-const extpay = ExtPay('pdf-bookmark')
-
-window.bears = {}
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  window.bears[request.url] = request.count
-})
-
-chrome.browserAction.onClicked.addListener(function (tab) {
+chrome.action.onClicked.addListener(function (tab) {
   chrome.tabs.create({url: 'popup.html'})
 })
