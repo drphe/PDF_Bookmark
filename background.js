d8efc41841d921b8e04b23d2243f62dbedd224be
drphe <mmail.phe@gmail.com>
2024-01-01 22:30:31 +0700

    ver 1.2.4

--- background.js
+++ background.js
@@ -1,3 +1,37 @@
-chrome.action.onClicked.addListener(function (tab) {
-  chrome.tabs.create({url: 'popup.html'})
+const HOST = "https://vi.zlibrary-global.se/s/?q=";
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        title: "Tìm trên z-library",
+        id: "searchPDF",
+        contexts: ["selection"]
+    })
+});
+chrome.contextMenus.onClicked.addListener(({
+    menuItemId,
+    selectionText
+}) => {
+    if (menuItemId === 'searchPDF') {
+        var o = selectionText.trim();
+        var dest = HOST + encodeURI(o);
+        chrome.tabs.create({
+            url: dest
+        });
+
+    }
+});
+
+
+chrome.action.onClicked.addListener(function(tab) {
+    chrome.tabs.create({
+        url: 'popup.html'
+    })
 })
+chrome.runtime.onMessage.addListener((req, sender, res) => {
+    if (req.action === "open_new_tab") {
+        chrome.tabs.create({
+            url: req.url
+        });
+        res({});
+    }
+});
---
+++ contentScript.js
@@ -0,0 +1,96 @@
+const HOST = "https://vi.zlibrary-global.se/s/?q=";
+
+const openNewTab = (fetchBookName) => {
+  const dest = fetchBookName();
+  chrome.runtime.sendMessage({ action: "open_new_tab", url: dest }, (r) => {});
+};
+
+const createButton = () => {
+  const btn = document.createElement("button");
+  btn.textContent = "Tìm kiếm PDF";
+  btn.style.padding = ".9rem";
+  btn.style.backgroundColor = "#337ab7";
+  btn.style.borderRadius = "3rem";
+  btn.style.color = "white";
+  btn.style.cursor = "pointer";
+  btn.style.border = ".1rem solid #337ab7";
+  btn.style.fontSize = "initial";
+  btn.style.fontWeight = "600";
+  btn.style.display = "block";
+
+  return btn;
+};
+
+const get_parent = () => {
+  const possible_parents = [
+    // document.querySelector(
+    //   "ul.a-unordered-list.a-nostyle.a-button-list.a-horizontal"
+    // ),
+    document.querySelector("#title"),
+  ];
+
+  return possible_parents.find((el) => !!el);
+};
+
+const is_amazon_book_page = () =>
+  document
+    .querySelector("#nav-global-location-data-modal-action")
+    .attributes[2].value.search("books") !== -1 ||
+  document
+    .querySelector("#nav-global-location-data-modal-action")
+    .attributes[2].value.search("audible") !== -1 ||
+  document.querySelector("#add-to-ebooks-cart-button");
+
+const amazon_addbtn = (fetchBookName) => {
+  if (is_amazon_book_page()) {
+    let parent = get_parent();
+    if (parent) {
+      const btn = createButton();
+      btn.addEventListener("click", () => openNewTab(fetchBookName));
+      parent.appendChild(btn);
+    }
+  }
+};
+
+const amazon_bookname = () => {
+  const bookName = document.querySelector("#productTitle").textContent.trim();
+
+  const dest = HOST + encodeURI(bookName);
+
+  return dest;
+};
+
+const goodreads_addbtn = (fetchBookName) => {
+  const ul = document.querySelector(".BookActions");
+  if (ul) {
+    const btn = createButton();
+    btn.addEventListener("click", () => openNewTab(fetchBookName));
+    ul.prepend(btn);
+  }
+};
+
+const goodreads_bookname = () => {
+  const bookName = document.querySelector("h1.Text__title1").textContent.trim();
+  const dest = HOST + encodeURI(bookName);
+  return dest;
+};
+
+const init = () => {
+  const host = window.location.hostname.split(".")[1];
+
+  switch (host) {
+    case "goodreads":
+      return [goodreads_addbtn, goodreads_bookname];
+    case "amazon":
+      return [amazon_addbtn, amazon_bookname];
+  }
+  return [null, null];
+};
+
+(() => {
+  const [addButton, fetchBookName] = init();
+
+  if (addButton && fetchBookName) {
+    addButton(fetchBookName);
+  }
+})();
