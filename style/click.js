7499b911382b4283023e39a689ed06b2d7c23b89
drphe <mmail.phe@gmail.com>
2024-01-26 12:18:57 +0700

    ver 1.3.6

--- style/click.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("click", function(event) {
-  var modalContainers = document.querySelectorAll(".modal-content.box.is-medium");
-  for (var i = 0; i < modalContainers.length; i++) {
-    if (!modalContainers[i].classList.contains("is-paddingless")) {
-      // Truy cập phần tử cha (div) và xóa nó
-      //modalContainers[i].parentElement.remove();
-    }
-  }
-});
